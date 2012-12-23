ca392e4a875607947352f195ee3bded0c143878e
Mario Jakopec <mario@faveo.hr>
2012-12-23 21:55:42 +0100

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Invoice::Application.config.secret_token = '817519e92181fc261681221265d0e3ef239b9065ab420a375a1d246b437d748f219efbd5ff5bc4b2fca8f464518c591eea78a9897f418b058f0235e43d021676'
